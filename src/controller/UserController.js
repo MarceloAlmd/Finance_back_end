5504dbd6e1833b8917bc7e63567b6d2b5a29d6f8
Marcelo <marcelo3x4@hotmail.com>
2023-01-20 07:01:17 -0300

    exceptions

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,7 +1,7 @@
+const AppError = require("../utils/AppError")
 class UserController {
     async create(request, response) {
-        const {name, email, password} = request.body;
-        
+        const {name, email, password} = request.bodyxxx;
         response.json({name, email, password})
     }
 }
