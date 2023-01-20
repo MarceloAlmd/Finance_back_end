8f12d6bd431ee4eeea2d448bb3f2da64f0d5a28a
Marcelo <marcelo3x4@hotmail.com>
2023-01-20 20:28:12 -0300

    router create user created

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,9 +1,30 @@
-const AppError = require("../utils/AppError")
+const {hash, compare} = require("bcryptjs");
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 class UserController {
     async create(request, response) {
         const {name, email, password} = request.body;
-        response.json({name, email, password})
-    }
-}
 
-module.exports = UserController 
+        const emailCheck = await knex("users").where({email}).first();
+
+        if(emailCheck) {
+            throw new AppError("Este e-mail já está em uso !");
+        }
+
+        const hashPassword = await hash(password, 8);
+
+        await knex("users").insert({
+            name,
+            email,
+            password: hashPassword
+        });
+
+        return response.status(201).json({
+            status: "201",
+            message: "Usuário criado com sucesso !"
+        });
+
+    };
+};
+
+module.exports = UserController;
