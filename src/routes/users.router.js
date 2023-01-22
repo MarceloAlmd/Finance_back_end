7fc81176ab3e14e17640085ad91701acbc070181
Marcelo <marcelo3x4@hotmail.com>
2023-01-22 19:29:03 -0300

    avatar router upload img database

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,14 +1,20 @@
 const {Router} = require("express");
+const multer = require("multer")
+const uploadConfig = require("../config/upload")
 
 const UserController = require("../controller/UserController");
+const UserAvatarController = require("../controller/UserAvatarController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
 const routesUsers = Router();
+const upload = multer(uploadConfig.MULTER);
 
-const userController = new UserController;
+const userController = new UserController()
+const userAvatarController = new UserAvatarController()
 
 routesUsers.post("/", userController.create);
 routesUsers.put("/", ensureAuthenticated, userController.update);
+routesUsers.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = routesUsers;
     
