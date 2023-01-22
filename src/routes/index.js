95db16523fe1ee3ccdf16272f0948cbf17fec689
Marcelo <marcelo3x4@hotmail.com>
2023-01-21 23:20:39 -0300

    created router user authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const {Router} = require("express");
 
 const routerUsers = require("./users.router")
 const routerFinance = require("./finance.router")
+const routerSession = require("./session.router")
 
 const routes = Router()
 
 routes.use("/users", routerUsers)
 routes.use("/finance", routerFinance)
+routes.use("/sessions", routerSession)
 
 module.exports = routes
