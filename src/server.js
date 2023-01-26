d2318f290e280c8cf57a8cce98f47636d8053176
Marcelo <marcelo3x4@hotmail.com>
2023-01-25 22:06:07 -0300

    test

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ app.use(cors())
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes);
-
 database()
 
 app.use((error, request, response, next) => {
